b2919cb70e53000bd41c24020d03b3594211db16
Martin Vanbrabant <martin.vanbrabant@ugent.be>
2022-02-08 17:45:36 +0100

    update

--- copy-config.sh
+++ copy-config.sh
@@ -1,3 +1,4 @@
+cp /var/www/html/patience.html ./var/www/html/
 cp /var/www/html/Skosmos/config.ttl ./var/www/html/Skosmos/
 cp /var/www/html/Skosmos/resource/translations/skosmos_nl.po ./var/www/html/Skosmos/resource/translations/
 cp /etc/apache2/sites-available/000-default.conf ./etc/apache2/sites-available/
